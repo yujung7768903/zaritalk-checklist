6343c195c16701eebd47c404b8697861442161de
yujung7768903 <yu-jung31476@naver.com>
2026-06-08 02:36:30 +0900

    chore: /api/v1 제거한 거에 맞춰 WebConfig.java 도 수정

--- backend/api/src/main/java/com/zaritalk/api/config/WebConfig.java
+++ backend/api/src/main/java/com/zaritalk/api/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
